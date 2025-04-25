a8345e022417e9faeacaa9aaadeedd1d44c30e22
Nikos Thomadakis <Nikos Thomadakis>
2025-04-26 02:57:47 +0300

    Example commit 2

--- Services/Discount/Discount.Grpc/Program.cs
+++ Services/Discount/Discount.Grpc/Program.cs
@@ -11,6 +11,8 @@ builder.Services.AddDbContext<DiscountContext>(opts =>
 
 var app = builder.Build();
 
+var asdasd = "asdasda";
+
 // Configure the HTTP request pipeline.
 app.UseMigration();
 app.MapGrpcService<DiscountService>();
